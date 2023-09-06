fe23b35601664952ba06ecefa90018100d35e3ca
Antanas Domarkas <antanas.domarkas@curonia.lt>
2023-09-06 05:45:41 +0300

    finish functions project

--- rust-book/04_functions/src/main.rs
+++ rust-book/04_functions/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+  println!("Hello, world!");
+
+  another_function(five(), 'h');
+}
+
+fn another_function(value: i32, unit_label: char) {
+  println!("The measurement is: {value}{unit_label}");
+}
+
+fn five() -> i32 {
+  5
 }
